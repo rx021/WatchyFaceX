62325d90235cf676103d424144c8597a8c7ec434
reinhardt <reinhardt.cgr@proton.me>
2025-08-16 23:16:24 -0700

    main: trying extracting the drawing; see if that works for dynamic changing

---
+++ face0.h
@@ -0,0 +1,22 @@
+void WatchyFaceX::drawFace0(bool enableDarkMode) {
+  display.fillScreen(enableDarkMode ? GxEPD_DARKGREY : GxEPD_LIGHTGREY);
+  display.setTextColor(enableDarkMode ? GxEPD_WHITE : GxEPD_BLACK);
+  display.setFont(&DSEG7_Classic_Bold_25);
+  display.setCursor(5, 110);
+
+  String timeString = "";
+
+  if (currentTime.Hour < 10){
+    timeString += "0";
+  }
+  timeString += currentTime.Hour;
+  timeString += ":";
+
+  if (currentTime.Minute < 10){
+    timeString += "0";
+  }
+  timeString += currentTime.Minute;
+  timeString += "!@#$%^&*()_-=+[]{}\\|:;'<,>.?/";
+
+  display.println(timeString);
+}
--- main.h
+++ main.h
@@ -13,8 +13,11 @@ class WatchyFaceX : public Watchy{
   using Watchy::Watchy;
   public: 
     void drawWatchFace();
+    void drawFace0(bool enableDarkMode);
     virtual void handleButtonPress(); // Must be virtual in Watchy.h too
 };
+
+#include "face0.h"
 
 void WatchyFaceX::handleButtonPress() {
   if (guiState == WATCHFACE_STATE) {
@@ -51,26 +54,7 @@ void WatchyFaceX::handleButtonPress() {
 
 void WatchyFaceX::drawWatchFace() {
   // TODO: extract this logic to separte watchface
-  display.fillScreen(isDarkMode ? GxEPD_DARKGREY : GxEPD_LIGHTGREY);
-  display.setTextColor(isDarkMode ? GxEPD_WHITE : GxEPD_BLACK);
-  display.setFont(&DSEG7_Classic_Bold_25);
-  display.setCursor(5, 110);
-
-  String timeString = "";
-
-  if (currentTime.Hour < 10){
-    timeString += "0";
-  }
-  timeString += currentTime.Hour;
-  timeString += ":";
-
-  if (currentTime.Minute < 10){
-    timeString += "0";
-  }
-  timeString += currentTime.Minute;
-  timeString += "!@#$%^&*()_-=+[]{}\\|:;'<,>.?/";
-
-  display.println(timeString);
+  drawFace0(isDarkMode);
 }                                         
 
 #endif
