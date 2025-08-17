e0aacbe4ec72f14acf5378164e4221690ea2e746
reinhardt <reinhardt.cgr@proton.me>
2025-08-17 00:42:08 -0700

    main: indentation fix; black and white since grey doesn't work

--- face0.h
+++ face0.h
@@ -3,8 +3,9 @@ const uint8_t BATTERY_SEGMENT_HEIGHT = 11;
 const uint8_t BATTERY_SEGMENT_SPACING = 9;
 
 void WatchyFaceX::drawFace0(bool enableDarkMode) {
-  // TODO: test that you use black and white instead
-  display.fillScreen(enableDarkMode ? GxEPD_DARKGREY : GxEPD_LIGHTGREY);
+  // toggle appears to work now; but greys don't work
+  // test black and white instead
+  display.fillScreen(enableDarkMode ? GxEPD_BLACK : GxEPD_WHITE);
 
   display.setTextColor(enableDarkMode ? GxEPD_WHITE : GxEPD_BLACK);
   display.setFont(&DSEG7_Classic_Bold_25);
@@ -36,26 +37,26 @@ void WatchyFaceX::drawFace0(bool enableDarkMode) {
 }
 
 void WatchyFaceX::drawBattery(bool enableDarkMode){
-    display.drawBitmap(158, 73, battery, 37, 21, enableDarkMode ? GxEPD_WHITE : GxEPD_BLACK);
-    display.fillRect(163, 78, 27, BATTERY_SEGMENT_HEIGHT, enableDarkMode ? GxEPD_BLACK : GxEPD_WHITE);//clear battery segments
+  display.drawBitmap(158, 73, battery, 37, 21, enableDarkMode ? GxEPD_WHITE : GxEPD_BLACK);
+  display.fillRect(163, 78, 27, BATTERY_SEGMENT_HEIGHT, enableDarkMode ? GxEPD_BLACK : GxEPD_WHITE);//clear battery segments
 
-    int8_t batteryLevel = 0;
-    float VBAT = getBatteryVoltage();
-    if(VBAT > 4.0){
-        batteryLevel = 3;
-    }
-    else if(VBAT > 3.6 && VBAT <= 4.0){
-        batteryLevel = 2;
-    }
-    else if(VBAT > 3.20 && VBAT <= 3.6){
-        batteryLevel = 1;
-    }
-    else if(VBAT <= 3.20){
-        batteryLevel = 0;
-    }
+  int8_t batteryLevel = 0;
+  float VBAT = getBatteryVoltage();
+  if(VBAT > 4.0){
+      batteryLevel = 3;
+  }
+  else if(VBAT > 3.6 && VBAT <= 4.0){
+      batteryLevel = 2;
+  }
+  else if(VBAT > 3.20 && VBAT <= 3.6){
+      batteryLevel = 1;
+  }
+  else if(VBAT <= 3.20){
+      batteryLevel = 0;
+  }
 
-    for(int8_t batterySegments = 0; batterySegments < batteryLevel; batterySegments++){
-        display.fillRect(163 + (batterySegments * BATTERY_SEGMENT_SPACING), 78, BATTERY_SEGMENT_WIDTH, BATTERY_SEGMENT_HEIGHT, enableDarkMode ? GxEPD_WHITE : GxEPD_BLACK);
-    }
+  for(int8_t batterySegments = 0; batterySegments < batteryLevel; batterySegments++){
+      display.fillRect(163 + (batterySegments * BATTERY_SEGMENT_SPACING), 78, BATTERY_SEGMENT_WIDTH, BATTERY_SEGMENT_HEIGHT, enableDarkMode ? GxEPD_WHITE : GxEPD_BLACK);
+  }
 }
 
