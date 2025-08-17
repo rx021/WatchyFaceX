f7108681fa666d488c115cf07c4e961424ffb3ba
reinhardt <reinhardt.cgr@proton.me>
2025-08-16 20:41:51 -0700

    main: trying new font and trying button press for darkmode

--- watchyFace250815-2300.ino
+++ watchyFace250815-2300.ino
@@ -2,18 +2,21 @@
 
 // font to use
 #include <Fonts/FreeMonoOblique24pt7b.h>
+#include "DSEG7_Classic_Bold_25.h"
                                          
 #include "settings.h"
 
 #define DARKMODE false
 
+RTC_DATA_ATTR int darkmode = 0
 
 class WatchyFace2508152300 : public Watchy{
   public: 
     WatchyFace2508152300(const watchySettings& s) : Watchy(s) {}
     void drawWatchFace(){
-      display.fillScreen(DARKMODE ? GxEPD_DARKGREY : GxEPD_LIGHTGREY);
-      display.setFont(&FreeMonoOblique24pt7b);
+      display.fillScreen(darkmode == 1 ? GxEPD_DARKGREY : GxEPD_LIGHTGREY);
+      display.setTextColor(darkmode == 1 ? GxEPD_WHITE : GxEPD_BLACK);
+      display.setFont(&DSEG7_Classic_Bold_25);
       display.setCursor(25, 110);
       if (currentTime.Hour < 10){
         display.print("0");
@@ -25,7 +28,39 @@ class WatchyFace2508152300 : public Watchy{
       }
       display.print(currentTime.Minute);
     }
+    virtual void handleButtonPress(); // Must be virtual in Watchy.h too
 };
+
+void WatchyFace2508152300::handleButtonPress() {
+  if (guiState == WATCHFACE_STATE) {
+    uint64_t wakeupBit = esp_sleep_get_ext1_wakeup_status();
+
+    if (wakeupBit && UP_BTN_MASK) {
+      Watchy::handleButtonPress();
+      return;
+    }
+
+    if (wakeupBit && DOWN_BTN_MASK) {
+      Watchy::handleButtonPress();
+      return;
+    }
+
+    if (wakeupBit && BACK_BTN_MASK) {
+      darkmode = (darkmode == 0) ? 1 : 0;
+      RTC.read(currentTime)
+      showWatchFace(true);
+      return;
+    }
+
+    if (wakeupBit && MENU_BTN_MASK) {
+      Watchy::handleButtonPress();
+      return;
+    }
+
+  } else {Watchy::handleButtonPress();}
+
+  return;
+}
 
 WatchyFace2508152300 m(settings); //instantiate watch face
 
