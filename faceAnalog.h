b3070421b5d3ea22d1390c3f56fa7cea41d8d57d
reinhardt <reinhardt.cgr@proton.me>
2025-08-17 16:02:33 -0700

    main: simplifying what I can

--- faceAnalog.h
+++ faceAnalog.h
@@ -21,31 +21,33 @@ void WatchyFaceX::drawFaceAnalog(bool enableDarkMode) {
   // draw minute scale
   radius = 98;
   for (uint8_t i=0; i<60; i++) {
-    display.drawLine(100, 100, 100+radius*cos(angle_minuteScale*i), 100+radius*sin(angle_minuteScale*i), light ? GxEPD_BLACK : GxEPD_WHITE);
+    display.drawLine(
+      100,
+      100,
+      100+radius*cos(angle_minuteScale*i),
+      100+radius*sin(angle_minuteScale*i),
+      textColor
+    );
   }
-  display.fillCircle(100, 100, 93, light ? GxEPD_WHITE : GxEPD_BLACK);
+  display.fillCircle(100, 100, 93, bgColor);
 
   // draw hour scale
   radius = 98;
   for (uint8_t i=0; i<12; i++) {
-    drawHand (radius, angle_hourScale*i, light);
+    drawHand(radius, angle_hourScale*i, light);
   }
-  display.fillCircle(100, 100, 88, light ? GxEPD_WHITE : GxEPD_BLACK);
+  display.fillCircle(100, 100, 88, bgColor);
 
   // draw hour hand
-  //radius = 45;
-  //drawHand (radius, angle_hourHand, light);
+  radius = 45;
+  drawHand(radius, angle_hourHand, light);
 
   // draw minute hand
   radius = 98;
-  drawHand (radius, angle_minuteHand, light);
+  drawHand(radius, angle_minuteHand, light);
 
   // draw center point
-  display.fillCircle(100, 100, 45, light ? GxEPD_WHITE : GxEPD_BLACK);
-
-
-
-
+  display.fillCircle(100, 100, 45, bgColor);
 }
 
 // helper function for hands drawing
