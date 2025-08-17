59ccf8954d9f88ba8d15157262969f92a0386158
reinhardt <reinhardt.cgr@proton.me>
2025-08-16 22:03:52 -0700

    main: including Watchy reference; extracted drawWatchFace to reduce nesting

--- watchyFace250815-2300.ino
+++ watchyFace250815-2300.ino
@@ -11,24 +11,10 @@
 RTC_DATA_ATTR int darkmode = 0;
 
 class WatchyFace2508152300 : public Watchy{
+  using Watchy::Watchy;
   public: 
     WatchyFace2508152300(const watchySettings& s) : Watchy(s) {}
-    void drawWatchFace(){
-      bool isDarkmode = (darkmode == 1);
-      display.fillScreen(isDarkmode ? GxEPD_DARKGREY : GxEPD_LIGHTGREY);
-      display.setTextColor(isDarkmode ? GxEPD_WHITE : GxEPD_BLACK);
-      display.setFont(&DSEG7_Classic_Bold_25);
-      display.setCursor(25, 110);
-      if (currentTime.Hour < 10){
-        display.print("0");
-      }
-      display.print(currentTime.Hour);
-      display.print(":");
-      if (currentTime.Minute < 10){
-        display.print("0");
-      }
-      display.print(currentTime.Minute);
-    }
+    void drawWatchFace();
     virtual void handleButtonPress(); // Must be virtual in Watchy.h too
 };
 
@@ -68,6 +54,23 @@ void WatchyFace2508152300::handleButtonPress() {
   Watchy::handleButtonPress();
 
   return;
+}
+
+void WatchyFace2508152300::drawWatchFace() {
+  bool isDarkmode = (darkmode == 1);
+  display.fillScreen(isDarkmode ? GxEPD_DARKGREY : GxEPD_LIGHTGREY);
+  display.setTextColor(isDarkmode ? GxEPD_WHITE : GxEPD_BLACK);
+  display.setFont(&DSEG7_Classic_Bold_25);
+  display.setCursor(25, 110);
+  if (currentTime.Hour < 10){
+    display.print("0");
+  }
+  display.print(currentTime.Hour);
+  display.print(":");
+  if (currentTime.Minute < 10){
+    display.print("0");
+  }
+  display.print(currentTime.Minute);
 }
 
 WatchyFace2508152300 m(settings); //instantiate watch face
