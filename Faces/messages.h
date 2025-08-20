d71713d76fc4cf8bbd0a0cde4d1a04ff744bf335
Reinhardt <reinhardt.cgr@gmail.com>
2025-08-19 23:48:18 -0700

    main: better variable name

--- Faces/messages.h
+++ Faces/messages.h
@@ -12,43 +12,43 @@ void WatchyFaceX::drawFaceMessages(bool enableDarkMode) {
 
   int16_t  x1, y1;
   uint16_t width, height;
-  String timeString = "FreeSans9pt7b";
+  String message = "";
   
   // TEST 01 - [' ]
   display.setFont(&FreeSans9pt7b);
-  timeString = "FreeSans9pt7b";
-  display.getTextBounds(timeString, 0, 0, &x1, &y1, &width, &height);
+  message = "FreeSans9pt7b";
+  display.getTextBounds(message, 0, 0, &x1, &y1, &width, &height);
   uint8_t msg1x = PADDING_X;
   uint8_t msg1y = PADDING_Y + height;
   display.setCursor(msg1x, msg1y);
-  display.print(timeString);
+  display.print(message);
                           
   // TEST 02 - [ ']
   display.setFont(&FreeSans12pt7b);
-  timeString = "FreeSans12pt7b";
-  display.getTextBounds(timeString, 0, 0, &x1, &y1, &width, &height);
+  message = "FreeSans12pt7b";
+  display.getTextBounds(message, 0, 0, &x1, &y1, &width, &height);
   uint8_t msg2x = DISPLAY_WIDTH - PADDING_X;
   uint8_t msg2y = msg1y + PADDING_Y + height;
   display.setCursor(msg2x, msg2y);
-  display.print(timeString);
+  display.print(message);
 
   // TEST 03 - [. ]
   display.setFont(&FreeSans18pt7b);
-  timeString = "FreeSans18pt7b";
-  display.getTextBounds(timeString, 0, 0, &x1, &y1, &width, &height);
+  message = "FreeSans18pt7b";
+  display.getTextBounds(message, 0, 0, &x1, &y1, &width, &height);
   uint8_t msg3x = PADDING_X;
   uint8_t msg3y = DISPLAY_HEIGHT - PADDING_Y;
   uint8_t msg3height = height;
   display.setCursor(msg3x, msg3y);
-  display.print(timeString);
+  display.print(message);
 
   // TEST 04 - [ .]
   display.setFont(&FreeSans24pt7b);
-  timeString = "FreeSans24pt7b";
-  display.getTextBounds(timeString, 0, 0, &x1, &y1, &width, &height);
+  message = "FreeSans24pt7b";
+  display.getTextBounds(message, 0, 0, &x1, &y1, &width, &height);
   uint8_t msg4x = DISPLAY_WIDTH - PADDING_X;
   uint8_t msg4y = msg3y - msg3height - PADDING_Y;
   display.setCursor(msg4x, msg4y);
-  display.print(timeString);
+  display.print(message);
 }
 
