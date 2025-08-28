bd0c793707d7c82c53f7c19d98c90e79ef8db39f
Reinhardt <reinhardt.cgr@gmail.com>
2025-08-27 18:10:39 -0700

    main: cleanup ocmmented out notes

--- Faces/messages.h
+++ Faces/messages.h
@@ -66,42 +66,5 @@ void WatchyFaceX::drawFaceMessages(bool enableDarkMode) {
   if (currentTime.Minute < 10) {timeString += "0";}
   timeString += currentTime.Minute;
   display.print(timeString);
-  
-  // TEST 01 - [' ]
-  //display.setFont(&FreeSans9pt7b);
-  //message = "FreeSans9pt7b";
-  //display.getTextBounds(message, 0, 0, &x1, &y1, &width, &height);
-  //uint8_t msg1x = PADDING_X;
-  //uint8_t msg1y = PADDING_Y + height;
-  //display.setCursor(msg1x, msg1y);
-  //display.print(message);
-                          
-  // TEST 02 - [ ']
-  //display.setFont(&FreeSans12pt7b);
-  //message = "FreeSans12pt7b";
-  //display.getTextBounds(message, 0, 0, &x1, &y1, &width, &height);
-  //uint8_t msg2x = DISPLAY_WIDTH - PADDING_X;
-  //uint8_t msg2y = msg1y + PADDING_Y + height;
-  //display.setCursor(msg2x, msg2y);
-  //display.print(message);
-
-  // TEST 03 - [. ]
-  //display.setFont(&FreeSans18pt7b);
-  //message = "FreeSans18pt7b";
-  //display.getTextBounds(message, 0, 0, &x1, &y1, &width, &height);
-  //uint8_t msg3x = PADDING_X;
-  //uint8_t msg3y = DISPLAY_HEIGHT - PADDING_Y;
-  //uint8_t msg3height = height;
-  //display.setCursor(msg3x, msg3y);
-  //display.print(message);
-
-  // TEST 04 - [ .]
-  //display.setFont(&FreeSans24pt7b);
-  //message = "FreeSans24pt7b";
-  //display.getTextBounds(message, 0, 0, &x1, &y1, &width, &height);
-  //uint8_t msg4x = DISPLAY_WIDTH - PADDING_X;
-  //uint8_t msg4y = msg3y - msg3height - PADDING_Y;
-  //display.setCursor(msg4x, msg4y);
-  //display.print(message);
 }
 
