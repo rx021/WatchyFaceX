6fb88f2c95f673f1181b4af8f55b34762f65a160
Reinhardt <reinhardt.cgr@gmail.com>
2025-09-01 13:37:36 -0700

    main: adding basic logic from showAccelerometer and hopefully can make this work

--- Faces/pinball.h
+++ Faces/pinball.h
@@ -1,3 +1,27 @@
 
 void WatchyFaceX::drawFacePinball(bool enableDarkMode) {
+  uint16_t bgColor = enableDarkMode ? GxEPD_BLACK : GxEPD_WHITE; 
+  uint16_t textColor = enableDarkMode ? GxEPD_WHITE : GxEPD_BLACK; 
+
+  display.setFullWindow();
+  display.fillScreen(bgColor);
+  display.setTextColor(textColor);
+
+  Accel acc;
+
+  long previousMillis = 0;
+  long interval = 200;
+
+  while (1) {
+    unsigned long currentMillis = millis();
+
+    if (digitalRead(BACK_BTN_PIN) == ACTIVE_LOW) {
+      break;
+    }
+
+    if (currentMillis - previousMillis > interval) {
+
+    }
+  }
+
 }
