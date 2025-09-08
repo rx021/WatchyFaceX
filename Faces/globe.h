a7575c6ade890feb98b78b48228e336f0f450403
Reinhardt <reinhardt.cgr@gmail.com>
2025-09-07 23:55:16 -0700

    feature-face: created loop to see all the frames of the globe

--- Faces/globe.h
+++ Faces/globe.h
@@ -9,6 +9,37 @@ void WatchyFaceX::drawFaceGlobe(
   display.setFullWindow();
   display.fillScreen(bgColor);
 
-  display.drawBitmap(0, 0, globe01, 200, 200, textColor);
+  auto drawNavigationFrame = [&]() {
+    display.drawBitmap(0, 0, globe01, 200, 200, textColor);
+    display.display(true); // full refresh
+  };
+
+  if (!enableInteractive) {
+    drawNavigationFrame();
+    return;
+  }
+
+  long lastMs = 0;
+  long updateIntervalMs = 100;
+
+  while (1) {
+    unsigned long now = millis();
+
+    // ACITVE_LOW (0 or 1) taken from Watchy github
+    if (digitalRead(BACK_BTN_PIN) == 0) { break; }
+    if ((now - lastMs) <= updateIntervalMs) { continue; }
+
+    // ACTION PER INTERVAL
+    lastMs = now;
+    display.fillScreen(bgColor);
+    display.drawBitmap(0, 0, globe02, 200, 200, textColor);
+    display.display(true); // full refresh
+                           
+    // only show initially so that we can switch between faces
+    if (!enableInteractive) { break; }
+  }
+
+  // After exiting game loop, show one navigation frame
+  drawNavigationFrame();
 }
 
