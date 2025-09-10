bacd933019571a079fbea8b74f7ba98fe1eac6c7
Reinhardt <reinhardt.cgr@gmail.com>
2025-09-09 23:36:28 -0700

    WF-CAL: rmoved unused margin

--- Faces/calendar.h
+++ Faces/calendar.h
@@ -3,7 +3,6 @@
 
 static const uint16_t WIDTH  = 200;
 static const uint16_t HEIGHT = 200;
-static const uint8_t  MARGIN = 8;
 static const uint8_t  GRID_TOP = 52;
 static const uint8_t  CELL_W = 26;
 static const uint8_t  CELL_H = 20;
