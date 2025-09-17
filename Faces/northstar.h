6ab2e5e8fadbd4ef46bc81fcdf5b150d064dbcbf
Reinhardt <reinhardt.cgr@gmail.com>
2025-09-17 00:56:36 -0700

    feature-simplify-face-types: adding date time to nortstar

--- Faces/northstar.h
+++ Faces/northstar.h
@@ -25,7 +25,6 @@ void WatchyFaceX::drawFaceNorthStar(
   // y1 is better to use instead of height here if we want the height of the font
   display.setCursor(mX, mY);
   display.println(message);
-  display.println("tyi96k twi1850 tdi370");
 
   display.setFont(&FreeSansBold9pt7b);
   display.println("dy-MDW_tests");
@@ -35,4 +34,37 @@ void WatchyFaceX::drawFaceNorthStar(
   display.println("wk-meetps_hcknghts");
   display.println("qr-12+1_wk_yr_exp");
   display.println("yr-DR_checkup");
+
+  // DRAW DATE from bottom-up: 
+
+  String dateString = "";
+  dateString += currentTime.Year + 1970; 
+  dateString += monthShortStr(currentTime.Month);
+  dateString += "-";
+
+  uint8_t currDay = currentTime.Day;
+  if (currDay < 10) { dateString += "0"; }
+  dateString += currDay;
+  dateString += dayShortStr(currentTime.Wday);
+
+  String timeString = ""; // must declare first to concat numbers
+  if (currentTime.Hour < 10) {timeString += "0";}
+  timeString += currentTime.Hour; // can add number to string
+  timeString += ":";
+  if (currentTime.Minute < 10) {timeString += "0";}
+  timeString += currentTime.Minute;
+
+  String datetimeString = dateString + "-" + timeString;
+
+  // bottomCenterText
+  int centerX = DISPLAY_WIDTH / 2;
+  int originY = DISPLAY_HEIGHT - PADDING_Y;
+
+  display.getTextBounds(datetimeString, 0, originY, &x1, &y1, &width, &height);
+
+  display.setCursor(centerX - (width/2), originY);
+  display.setFont(&Seven_Segment10pt7b);
+  display.setTextColor(textColor);
+  display.print(datetimeString);
+
 }
