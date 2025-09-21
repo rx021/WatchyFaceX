7435f5ee9fac11b690892f8dbae6f23ed0332960
Reinhardt <reinhardt.cgr@gmail.com>
2025-09-21 00:40:44 -0700

    main: added same time string to countdown face; let's see if it worked

--- Faces/countdown.h
+++ Faces/countdown.h
@@ -24,5 +24,14 @@ void WatchyFaceX::drawFaceCountdowwn(
 
   display.setCursor(mX, mY);
   display.println(message);
+
+  // DRAW TIMER NUMBERS
+  String timeString = "00:00:00";
+  display.getTextBounds(timeString, 0, 0, &x1, &y1, &w, &h);
+  uint8_t timeX = (DISPLAY_WIDTH / 2) - (w/2);
+  uint8_t timeY = (DISPLAY_HEIGHT / 2) + (h/2);
+  display.setFont(&DSEG7_Classic_Bold_25);
+  display.setCursor(timeX, timeY);
+  display.println(timeString);
 }
 
