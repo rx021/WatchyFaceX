3a9cbaa7e1c99dc766c04db165180e99e889cdc0
Reinhardt <reinhardt.cgr@gmail.com>
2025-09-21 01:11:37 -0700

    main: simplifying face code for timer with helper

--- Faces/timer.h
+++ Faces/timer.h
@@ -8,27 +8,20 @@ void WatchyFaceX::drawFaceTimer(
 
   display.setFullWindow();
   display.fillScreen(bgColor);
-  display.setFont(&FreeSansBold12pt7b);
-  display.setTextColor(textColor);
-
-  uint8_t PADDING_X = 1; // pixels
-  uint8_t PADDING_Y = 1; // pixels
-  int16_t  x1, y1;
-  uint16_t w, h;
 
   String title = "TIMER";
-  display.getTextBounds(title, 0, 0, &x1, &y1, &w, &h);
-  uint8_t mX = PADDING_X;
-  int8_t mY = PADDING_Y + (-1 *  y1);
-  // y1 is better to use instead of height here if we want the height of the font
+  drawFaceTitle(display, title, textColor);
 
-  display.setCursor(mX, mY);
-  display.println(title);
 
   // DRAW TIMER NUMBERS
   display.setFont(&DSEG7_Classic_Bold_25);
+  display.setTextColor(textColor);
   String timeString = "00:00:00";
+
+  int16_t  x1, y1;
+  uint16_t w, h;
   display.getTextBounds(timeString, 0, 0, &x1, &y1, &w, &h);
+
   uint8_t timeX = (DISPLAY_WIDTH / 2) - (w/2);
   uint8_t timeY = (DISPLAY_HEIGHT / 2) + (h/2);
   display.setCursor(timeX, timeY);
