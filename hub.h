67a409b491111ef92c742fa3d9ff07cee3df2888
reinhardt <reinhardt.cgr@proton.me>
2026-02-10 16:07:39 -0800

    fix-lower-memory: added basic logic to use the buttons

--- hub.h
+++ hub.h
@@ -29,11 +29,89 @@ class WatchyFaceX : public Watchy{
       bool enableDarkMode,
       bool enableInteractive
     );
+
+    // NOTE: Must be virtual in Watchy.h too
+    virtual void handleButtonPress();
 };
 
 // needs to be included after class declared
 //#include "Faces/calendar.h"
 #include "Faces/analog.h"
+
+void WatchyFaceX::handleButtonPress() {
+  if (guiState == WATCHFACE_STATE) {
+    uint64_t wakeupBit = esp_sleep_get_ext1_wakeup_status();
+
+    // NOTE: beware this condition 
+    // for buttons to work
+    if (wakeupBit & BACK_BTN_MASK) {
+      /*
+      // NOTE: order should match FACE_TYPES
+      if (faceTypeIndex == 0) {
+        plannerFacesIndex = (plannerFacesIndex + 1) % PLANNER_COUNT;
+      }
+      else if (faceTypeIndex == 1) {
+        noteFacesIndex = (noteFacesIndex + 1) % NOTE_COUNT;
+      }
+      else if (faceTypeIndex == 2) {
+        // reset when changing face
+        enableInteractive = false;
+        toyFacesIndex = (toyFacesIndex + 1) % TOY_COUNT;
+      }
+
+      RTC.read(currentTime);
+      showWatchFace(true);
+      */
+
+      Watchy::handleButtonPress(); //temp
+      return;
+    }
+
+    if (wakeupBit & MENU_BTN_MASK) {
+      /*
+      if (
+        faceTypeIndex == 2 // toys
+      ) {
+        enableInteractive = true;
+        RTC.read(currentTime);
+        showWatchFace(true);
+        return;
+      }
+      */
+
+      Watchy::handleButtonPress(); //temp
+      return;
+    }
+
+    if (wakeupBit & UP_BTN_MASK) {
+      /*
+      faceTypeIndex = (faceTypeIndex-1 < 0)
+        ? FACE_TYPE_COUNT-1
+        : faceTypeIndex-1;
+      // reset when changing face type
+      enableInteractive = false;
+      RTC.read(currentTime);
+      showWatchFace(true);
+      */
+      Watchy::handleButtonPress(); //temp
+      return;
+    }
+
+    if (wakeupBit & DOWN_BTN_MASK) {
+      /*
+      faceTypeIndex = (faceTypeIndex + 1) % FACE_TYPE_COUNT;
+      // reset when changing face type
+      enableInteractive = false;
+      RTC.read(currentTime);
+      showWatchFace(true);
+      */
+      Watchy::handleButtonPress(); //temp
+      return;
+    }
+
+  } else {Watchy::handleButtonPress();}
+  return;
+}
 
 //-- triggered by showWatchFace() 
 void WatchyFaceX::drawWatchFace() {
